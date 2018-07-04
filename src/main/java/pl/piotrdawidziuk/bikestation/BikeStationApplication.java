9b6aad5344220aee6544aa871a042cbaaf757ef2
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-07-04 17:26:28 +0200

    first commit, models started

---
+++ src/main/java/pl/piotrdawidziuk/bikestation/BikeStationApplication.java
@@ -0,0 +1,12 @@
+package pl.piotrdawidziuk.bikestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeStationApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/piotrdawidziuk/bikestation/model/BikeStation.java
@@ -0,0 +1,15 @@
+package pl.piotrdawidziuk.bikestation.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BikeStation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
