6695727e0f329e23570334065eb7fbadde82090d
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-07-04 19:44:27 +0200

    adding option added

--- src/main/java/pl/piotrdawidziuk/bikestation/controller/StationController.java
+++ src/main/java/pl/piotrdawidziuk/bikestation/controller/StationController.java
@@ -3,12 +3,13 @@ package pl.piotrdawidziuk.bikestation.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.piotrdawidziuk.bikestation.model.Station;
 import pl.piotrdawidziuk.bikestation.repository.StationRepository;
 
 @Controller
-public class BikeStationController {
+public class StationController {
 
     @Autowired
     StationRepository stationRepository;
@@ -20,4 +21,20 @@ public class BikeStationController {
         // This returns a JSON or XML with the users
         return stationRepository.findAll();
     }
+
+    @GetMapping(path="/add") // Map ONLY GET Requests
+    public @ResponseBody String addNewStation (@RequestParam String name) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        Station n = new Station();
+        n.setName(name);
+       // n.setId(10l);
+        stationRepository.save(n);
+        return "Saved";
+    }
+
+
+
+
 }
